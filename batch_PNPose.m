15d4a1b39deb41f6291ac47713f2778c3664f7ce
leehsiu <lixiulive@hotmail.com>
2018-05-22 18:35:56 -0400

    Batch PNP

--- batch_PNPose.m
+++ batch_PNPose.m
@@ -1,4 +1,4 @@
-% 3D data format
+% h36m data format
 % 1 (head)
 % 2 (neck)
 % 3 (right shoulder)
@@ -14,7 +14,7 @@
 % 13 (left knee)
 % 14 (left ankle)
 
-%H36m
+% MPI data format
 % 1 (head)       4
 % 2 (neck)       3
 % 3 (right shoulder)    13
@@ -31,8 +31,6 @@
 % 14 (left ankle)       10
 
 
-%%Load 2D data. Use cellfun to PnP input images and the library. Then the final pose is 
-
 addpath('EPnP_matlab/EPnP/');
 addpath('EPnP_matlab/error/');
 
@@ -41,50 +39,37 @@ load('~/databag/mpii_human_pose/h36m_icon.mat');
 
 mpi2h36m = [4 3 13 12 11 14 15 16 7 6 5 8 9 10];
 
+P = 14;
+
 %%
-F = length(MPI_anno);
+F_2d = length(MPI_anno);
 F_3d = length(h36m_icon);
 h36m_cell = cellfun(@transpose,h36m_icon,'UniformOutput',false);
 
+MPI_3Danno = zeros(F_2d,1);
+for idx=1:F_2d
+    c_anno = MPI_anno(idx);    
+    ptsX = cell2mat({c_anno.points.x});
+    ptsY = cell2mat({c_anno.points.y});
+    ptsX = ptsX(mpi2h36m);
+    ptsY = ptsY(mpi2h36m);
+    mpi_2dt = [ptsX' ptsY' ones(P,1)];    
+    K_cell = cell(F_3d,1);
+    Pts_cell = cell(F_3d,1);
+    K_cell(:) = {c_anno.image_K};
+    Pts_cell(:) = {mpi_2dt};             
+    [~,~,~,err]=cellfun(@efficient_pnp_error,h36m_cell,Pts_cell,K_cell,'UniformOutput',false);
+    [~,sol] = min(cell2mat(err));
+    MPI_3Danno(idx) = sol;     
+    fprintf('MPI 2D id: %d  3D id :%d\n',idx,sol);    
+end
+
+save('MPI_3Danno.mat','MPI_3Danno');
 
 
-idx = randi(F,1);
-img = imread(['~/databag/mpii_human_pose/images/' MPI_anno(idx).image]);    
-ptsX = cell2mat({MPI_anno(idx).points.x});
-ptsY = cell2mat({MPI_anno(idx).points.y});
-ptsX = ptsX(mpi2h36m);
-ptsY = ptsY(mpi2h36m);
-P = length(ptsX);
 
-mpi_2dt = [ptsX' ptsY' ones(P,1)];
 
-[h,w,~] = size(img);
-K = eye(3);
-fc = h;
 
-K(1,3) = w/2;
-K(2,3) = h/2;
-K(1,1) = fc;
-K(2,2) = fc;
-K_cell = cell(F_3d,1);
-K_cell(:) = {K};
-Pts_cell = cell(F_3d,1);
-Pts_cell(:) = {mpi_2dt};
 
-tic;
-[Rp,Tp,Xc,err]=cellfun(@efficient_pnp_error,h36m_cell,Pts_cell,K_cell,'UniformOutput',false);
-toc;
-err = cell2mat(err);
 
-[~,sol] = min(err);
-figure(1);
-clf;
-hold on;
-subplot(1,3,[1 2]);
-imshow(img);
-vis_2d(mpi_2dt);
-subplot(1,3,3)
-vis_3d(h36m_cell{sol});
-grid on;
-view([0 30]);
 
