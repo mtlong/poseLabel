e12e8da182a5194f234f32265123c78738121590
leehsiu <lixiulive@hotmail.com>
2018-05-25 16:35:20 -0400

    temp

--- Cut_MPI_h36m.m
+++ Cut_MPI_h36m.m
@@ -1,29 +1,30 @@
+%% Load data
 load('~/databag/mpii_human_pose/MPI_anno.mat');
 load('~/databag/mpii_human_pose/h36m_icon.mat');
 load('~/databag/mpii_human_pose/MPI_3Danno.mat');
 mpi2h36m = [4 3 13 12 11 14 15 16 7 6 5 8 9 10];
 P = 14;
 F_anno = length(MPI_3Danno);
-
-
 groupsize_Th = 10;
 
-%%
-num_h36m = length(h36m_icon);
-group_id = cell(num_h36m,1);
+normerrTh = 0.05;
 
-for i=1:num_h36m
-    group_id(i) = {find(MPI_3Danno(:,1)==i)};
+%% group size
+group_num = length(h36m_icon);
+group_id = cell(group_num,1);
+for i=1:group_num
+    group_id(i) = {find(MPI_3Danno(:,1)==i & MPI_3Danno(:,6)>normerrTh)};
 end
+
 
 group_size = cellfun(@length,group_id,'UniformOutput',true);
 select_group = find(group_size>groupsize_Th);
 system('rm -rf ~/databag/group_images');
 mkdir('~/databag/group_images/');
 
+f = figure('visible','off');
 %%
-for i=1:length(select_group)
-    
+for i=1:length(select_group)    
     mkdir(['~/databag/group_images/' int2str(select_group(i))]);
     mpi_idx = group_id{select_group(i)};
     for j=1:length(mpi_idx)        
@@ -33,8 +34,8 @@ for i=1:length(select_group)
         c_2d_x = c_2d_x(mpi2h36m);
         c_2d_y = c_2d_y(mpi2h36m);
         c_2d = [c_2d_x' c_2d_y' ones(P,1)];    
-        f = figure('visible','off');
-        clf;
+        figure(f);        
+        clf(f);
         hold on;
         imshow(img);
         vis_2d(c_2d,3,'-');
--- PCK_MPI_h36m.m
+++ PCK_MPI_h36m.m
@@ -8,13 +8,12 @@ mpi2h36m = [4 3 13 12 11 14 15 16 7 6 5 8 9 10];
 P = 14;
 
 F_anno = length(MPI_3Danno);
-
 h36m_cell = cellfun(@transpose,h36m_icon,'UniformOutput',false);
 
-Anno3D_cell = h36m_cell(MPI_3Danno(:,1));
+Anno3D_cell = h36m_cell(MPI_3Danno(:,1)); %%KNN = 1
 Anno2D_cell = cell(F_anno,1);
 
-%%
+%% Calc normalized reprojection error.
 K_cell = {MPI_anno.image_K}';
 
 for i=1:F_anno
@@ -27,14 +26,20 @@ end
 
 [~,~,~,err]=cellfun(@efficient_pnp_error,Anno3D_cell,Anno2D_cell,K_cell,'UniformOutput',false);
 err = cell2mat(err);
-%%
+
 Anno2D_bbox_lt = cellfun(@min,Anno2D_cell,'UniformOutput',false);
 Anno2D_bbox_rb = cellfun(@max,Anno2D_cell,'UniformOutput',false);
 Anno2D_bbox_d = cellfun(@minus,Anno2D_bbox_rb,Anno2D_bbox_lt,'UniformOutput',false);
 Anno2D_bbox_diag = cellfun(@norm,Anno2D_bbox_d);
+normerr = err./Anno2D_bbox_diag;
+
+MPI_3Danno(:,6) = normerr;
 
 %%
-normerr = err./Anno2D_bbox_diag;
+
+
+
+
 dist = 0:0.01:1;
 PCK = sum(normerr<dist)./F_anno;
 
