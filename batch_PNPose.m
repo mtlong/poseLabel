986b7f0eb3b036810c97c9b163b077ac95cc25d1
leehsiu <lixiulive@hotmail.com>
2018-05-25 17:27:59 -0400

    update PnPPose

--- batch_PNPose.m
+++ batch_PNPose.m
@@ -15,20 +15,26 @@
 % 14 (left ankle)
 
 % MPI data format
-% 1 (head)       4
-% 2 (neck)       3
-% 3 (right shoulder)    13
-% 4 (right elbow)       12
-% 5 (right wrist)       11
-% 6 (left shoulder)     14
-% 7 (left elbow)        15
-% 8 (left wrist)        16
-% 9 (right hip)         7
-% 10 (right knee)       6
-% 11 (right ankle)      5
-% 12 (left hip)         8
-% 13 (left knee)        9
-% 14 (left ankle)       10
+%1 - r ankle, 
+%2 - r knee, 
+%3 - r hip,
+%4 - l hip, 
+%5 - l knee, 
+%6 - l ankle, 
+%7 - pelvis, 
+%8 - thorax
+%9 - upper neck, 
+%10 - head top, 
+%11 - r wrist, 
+%12 - r elbow, 
+%13 - r shoulder, 
+%14 - l shoulder, 
+%15 - l elbow, 
+%16 - l wrist
+
+%MPI 2 h36m:
+% 10 9 13 12 11 14 15 16 3 2 1 4 5 6
+KNN = 5;
 
 
 addpath('EPnP_matlab/EPnP/');
@@ -37,35 +43,35 @@ addpath('EPnP_matlab/error/');
 load('~/databag/mpii_human_pose/MPI_anno.mat');
 load('~/databag/mpii_human_pose/h36m_icon.mat');
 
-mpi2h36m = [4 3 13 12 11 14 15 16 7 6 5 8 9 10];
+P = 16;
 
-P = 14;
-
-%%
+mpi2h36m = [10 9 13 12 11 14 15 16 3 2 1 4 5 6]';
+%% 
 F_2d = length(MPI_anno);
 F_3d = length(h36m_icon);
 h36m_cell = cellfun(@transpose,h36m_icon,'UniformOutput',false);
+MPI_3DAnno = zeros(F_2d,KNN*2);
 
-MPI_3Danno = zeros(F_2d,1);
-parfor idx=1:F_2d
-    c_anno = MPI_anno(idx);    
-    ptsX = cell2mat({c_anno.points.x});
-    ptsY = cell2mat({c_anno.points.y});
-    ptsX = ptsX(mpi2h36m);
-    ptsY = ptsY(mpi2h36m);
-    mpi_2dt = [ptsX' ptsY' ones(P,1)];    
+for idx=1:F_2d
+    c_anno = MPI_anno(idx).points;
+    pts = [c_anno.x;c_anno.y];
+    ptsid = [c_anno.id]+1;    
+    pts2d = zeros(size(pts));    
+    pts2d(:,ptsid) = pts; 
+    pts2d = [pts2d' ones(P,1)];
+    pts2d = pts2d(mpi2h36m,:);
+    
     K_cell = cell(F_3d,1);
     Pts_cell = cell(F_3d,1);
     K_cell(:) = {c_anno.image_K};
-    Pts_cell(:) = {mpi_2dt};             
+    Pts_cell(:) = {pts2d};
     [~,~,~,err]=cellfun(@efficient_pnp_error,h36m_cell,Pts_cell,K_cell,'UniformOutput',false);
-    [~,sol] = min(cell2mat(err));
-    MPI_3Danno(idx) = sol;     
-    fprintf('MPI 2D id: %d  3D id :%d\n',idx,sol);    
+    [sorterr,sortid] = sort(cell2mat(err));    
+    solid = sortid(1:KNN);
+    solerr = sorterr(1:KNN);
+    MPI_3DAnno(idx,:) = [solid,solerr];    
+    fprintf('MPI 2D id: %d \n',idx);    
 end
 
-save('MPI_3Danno.mat','MPI_3Danno');
-
-
-
+save('MPI_3DAnno.mat','MPI_3DAnno');
 
